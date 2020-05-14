ccd0c043ec8c9081f759c22595cd1b9fdbec377b
mimura <musyu36@gmail.com>
2020-05-14 14:27:28 +0900

    practice state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+// const App = () => {
+//   const profiles = [
+//     {
+//       name: "Taro",
+//       age: 10,
+//     },
+//     {
+//       name: "Mary",
+//       age: 20,
+//     },
+//     {
+//       name: "John",
+//       age: 3,
+//     },
+//   ];
+//   return (
+//     <div>
+//       {profiles.map((profile, index) => {
+//         return <User name={profile.name} age={profile.age} key={index} />;
+//       })}
+//     </div>
+//   );
+// };
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       I am {props.name}, {props.age} years old
+//     </div>
+//   );
+// };
+
+// User.defaultProps = {
+//   age: 1,
+// };
+
+// // Userコンポーネントが受け付けるpropsに対してprop-typesで型チェックをする
+// // isRequiredでnullチェック
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired,
+// };
 const App = () => {
-  const profiles = [
-    {
-      name: "Taro",
-      age: 10,
-    },
-    {
-      name: "Mary",
-      age: 20,
-    },
-    {
-      name: "John",
-      age: 3,
-    },
-  ];
   return (
     <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
+      <Counter />
     </div>
   );
 };
 
-const User = (props) => {
-  return (
-    <div>
-      I am {props.name}, {props.age} years old
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-User.defaultProps = {
-  age: 1,
-};
-
-// Userコンポーネントが受け付けるpropsに対してprop-typesで型チェックをする
-// isRequiredでnullチェック
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-};
+  handlePlusButton = () => {
+    // setStateを用いることでコールバックでrenderが実行される
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleMinusButton = () => {
+    // setStateを用いることでコールバックでrenderが実行される(stateの変更に伴って表示が変わる)
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
