10bb5e1c2494c6bfa43b487f4f8a9da8ad455ce9
mimura <musyu36@gmail.com>
2020-05-14 15:05:29 +0900

    practice store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import "./index.css";
+import reducer from "./reducers";
+
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+
+// storeはアプリ内で唯一で，全てのstateはstoreに集約
+const store = createStore(reducer);
+
+// ProviderでAppをラップし，storeを渡して置くことで全コンポーネントがstoreにアクセス出来る
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
